26070e9df54d6bc6998893df1950199f6b91ea04
Nicolas Carrasco <nicolas.carrasco@epitech.eu>
2022-11-17 15:38:24 +0100

    Create front folder

---
+++ pwa-front/src/themes/palette.theme.ts
@@ -0,0 +1,35 @@
+import { PaletteOptions } from '@mui/material';
+import Colors from '../constants/colors.constants';
+
+declare module '@mui/material/styles' {
+  interface Palette {
+    neutral: Palette['primary'];
+  }
+
+  interface PaletteOptions {
+    neutral: PaletteOptions['primary'];
+  }
+}
+
+const palette: PaletteOptions = {
+  primary: {
+    main: Colors.primary,
+  },
+  secondary: {
+    main: Colors.secondary,
+  },
+  neutral: {
+    main: Colors.white,
+  },
+  error: {
+    main: Colors.error,
+  },
+  background: {
+    default: 'rgb(249, 249, 249)',
+  },
+  text: {
+    secondary: Colors.secondaryText,
+  },
+};
+
+export default palette;
